63d3b02a4ca794a1e754d92b7c3498d43af1cc0d
shifin424 <muhammedshifin424@gmail.com>
2023-02-28 01:35:09 +0530

    edited

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const adminController = require('../controllers/adminController');
+const adminController = require('../Controllers/adminController');
 const { loginSession } = require('../middlewares/session');
 const adminRouter = express()
 const verifylogin  = require('../middlewares/session');
