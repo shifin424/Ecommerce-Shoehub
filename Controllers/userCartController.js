f03299b2f860328455e54e25b0ae06346348f2b6
shifin424 <muhammedshifin424@gmail.com>
2023-03-02 09:58:30 +0530

    checkout bug ,cart bug solved

--- Controllers/userCartController.js
+++ Controllers/userCartController.js
@@ -163,95 +163,85 @@ const removeProduct = async (req, res) => {
 };
 
 
-const totalAmount = async (req, res) => {
+
+
+
+
+
+
+const changeQuantity = async (req, res, next) => {
     try {
+      const data = req.body
+      console.log(data, "counted");
+      const objId = mongoose.Types.ObjectId(data.product)
 
-        let session = req.session.user;
-        const userData = await users.findOne({ email: session.email });
-        const productData = await cart.aggregate([
+      if (data.count == -1 && data.quantity == 1) {
+        cart.updateOne(
+          { _id: data.cart, "product.productId": objId },
+          { $pull: { product: { productId: objId } } }
+        )
+          .then(() => {
+            res.json({ quantity: true })
+          }).catch(err => console.log(err))
+      } else {
+        cart.updateOne(
+          { _id: data.cart, "product.productId": objId },
+          { $inc: { "product.$.quantity": data.count } }
+        ).then(async () => {
+          // Get updated product data
+          const productData = await cart.aggregate([
             {
-                $match: { userId: userData.id },
+              $match: { _id: mongoose.Types.ObjectId(data.cart) }
             },
             {
-                $unwind: "$product",
+              $unwind: "$product",
             },
             {
-                $project: {
-                    productItem: "$product.productId",
-                    productQuantity: "$product.quantity",
+              $project: {
+                productItem: "$product.productId",
+                productQuantity: "$product.quantity",
+              },
+            },
+            {
+              $lookup: {
+                from: "products",
+                localField: "productItem",
+                foreignField: "_id",
+                as: "productDetail",
+              },
+            },
+            {
+              $project: {
+                productItem: 1,
+                productQuantity: 1,
+                productDetail: { $arrayElemAt: ["$productDetail", 0] },
+              },
+            },
+            {
+              $addFields: {
+                productPrice: {
+                  $multiply: ["$productQuantity", "$productDetail.price"],
                 },
+              },
             },
             {
-                $lookup: {
-                    from: "products",
-                    localField: "productItem",
-                    foreignField: "_id",
-                    as: "productDetail",
+              $group: {
+                _id: null,
+                total: {
+                  $sum: { $multiply: ["$productQuantity", "$productDetail.price"] },
                 },
+              },
             },
-            {
-                $project: {
-                    productItem: 1,
-                    productQuantity: 1,
-                    productDetail: { $arrayElemAt: ["$productDetail", 0] },
-                },
-            },
-            {
-                $addFields: {
-                    productPrice: {
-                        $multiply: ["$productQuantity", "$productDetail.price"],
-                    },
-                },
-            },
-            {
-                $group: {
-                    _id: userData.id,
-                    total: {
-                        $sum: { $multiply: ["$productQuantity", "$productDetail.price"] },
-                    },
-                },
-            },
-        ]).exec();
-        res.json({ status: true, productData });
+          ]).exec();
+          console.log(productData);
+          res.json({ success: true, productData: productData })
+        })
+      }
     } catch (err) {
-        console.log(err);
-        next(err)
+      console.log(err);
+      next(err)
     }
-};
-
-
-
-
-
-const changeQuantity = (req, res, next) => {
-    try {
-        const data = req.body
-        console.log(data, "counted");
-        const objId = mongoose.Types.ObjectId(data.product)
-        
-
-        if (data.count == -1 && data.quantity == 1) {
-            cart.updateOne(
-                { _id: data.cart, "product.productId": objId },
-                { $pull: { product: { productId: objId } } }
-            )
-                .then(() => {
-                    res.json({ quantity: true })
-                }).catch(err => console.log(err))
-        } else {
-            cart.updateOne(
-                { _id: data.cart, "product.productId": objId },
-                { $inc: { "product.$.quantity": data.count } }
-            ).then(() => {
-                next()
-            })
-        }
-    } catch (err) {
-        console.log(err);
-        next(err)
-    }
-
-};
+  };
 
 
 
@@ -263,7 +253,7 @@ module.exports = {
     addToCart,
     removeProduct,
     changeQuantity,
-    totalAmount
+  
 
 
 }
